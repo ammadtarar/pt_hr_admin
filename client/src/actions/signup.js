df1992ecfcd8d8b01397411be6ccc7124d5acb49
Julien LUCAS <contact@julienlucas.com>
2019-10-02 17:39:33 +0200

    heroku deploy

--- client/src/actions/signup.js
+++
@@ -1,4 +0,0 @@
-export const signup = ({ clientKey } = {}) => ({
-  type: 'SIGNUP',
-  clientKey
-});
--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import AppRouter, { history } from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import { login, logout } from './actions/auth';
-import './firebase/firebase';
-import 'normalize.css/normalize.css';
-import style from 'foundation-sites/dist/css/foundation-float.min.css';
-import './styles/styles.scss';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = configureStore();
-store.dispatch(logout());
-
-const App = (
-  <Provider store={store}>
-    <AppRouter/>
-  </Provider>
-);
-
-ReactDOM.render(App, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
