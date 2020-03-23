ccbb6a4f96e639309a4c44024e5625097ee87b3f
Julien LUCAS <contact@julienlucas.com>
2020-03-23 14:39:56 +0100

    changes

--- client/src/components/CardCandidat.js
+++ client/src/components/CardCandidat.js
@@ -12,9 +12,16 @@ function CardCandidat(props) {
 
   return (
     <div className="box-encart" onMouseOver={(e) => setHover(data.id)} onMouseOut={(e) => setHover(false)}>
-      <div className={`icon ${data.archive === true ? 'athens-gray': ''}`}>{data.prenom.charAt(0) + data.nom.charAt(0)}</div>
-      <p className="nom">{data.prenom + ' ' + data.nom}</p>
-      <p className="titre">{data.titre}</p>
+      <div className="row-fluid box-nom-titre">
+        <div className="large-2 columns">
+          <div className={`icon ${data.archive === true ? 'athens-gray': ''}`}>{data.prenom.charAt(0) + data.nom.charAt(0)}</div>
+        </div>
+        <div className="large-10 columns">
+          <p className="nom">{data.prenom + ' ' + data.nom}</p>
+          <p className="titre">{data.titre}</p>
+        </div>
+      </div>
+
       <p className="cooptes-par">Coopté par&nbsp;
         {Object.keys(data.cooptations).map((key, i) => {
           const nbrCooptes = Object.keys(data.cooptations).length
