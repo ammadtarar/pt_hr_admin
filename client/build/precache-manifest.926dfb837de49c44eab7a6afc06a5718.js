1df2767cb0238b7584f4df6ca5f3c15bfdc07214
Ammad Amjad Tarar <info@sawatechnologies.org>
2021-02-23 22:18:20 +0500

    Working on production

---
+++ client/build/precache-manifest.926dfb837de49c44eab7a6afc06a5718.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d9a9423c4fafef2eb412eff2dd8e97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94186b9d86800e206f61",
+    "url": "/static/css/2.9e384dab.chunk.css"
+  },
+  {
+    "revision": "3f9065c8a81dc476eb3f",
+    "url": "/static/css/main.b81bf26b.chunk.css"
+  },
+  {
+    "revision": "94186b9d86800e206f61",
+    "url": "/static/js/2.b7695b57.chunk.js"
+  },
+  {
+    "revision": "f2c69026083a5163df84",
+    "url": "/static/js/3.039eddaa.chunk.js"
+  },
+  {
+    "revision": "c3f898a8b764bb72c6d2",
+    "url": "/static/js/4.169446a3.chunk.js"
+  },
+  {
+    "revision": "a2baf62d47063be28923",
+    "url": "/static/js/5.dcb0ff99.chunk.js"
+  },
+  {
+    "revision": "45d8fa58ae87c1386311",
+    "url": "/static/js/6.ed728c1d.chunk.js"
+  },
+  {
+    "revision": "3f9065c8a81dc476eb3f",
+    "url": "/static/js/main.343f789f.chunk.js"
+  },
+  {
+    "revision": "eb6f63294b708b865270",
+    "url": "/static/js/runtime-main.7414683a.js"
+  }
+]);
